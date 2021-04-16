649fb1b47794b8005f106dd3cc2a1367d5af04d4
ekuznetski <e.kuznetski@gmail.com>
2021-04-16 17:32:55 +0300

    [init] Setup base app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import "./styles.scss";
+
+function App() {
+  return <div>test</div>;
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    presets: ["@babel/polyfill"],
+    main: ["react-hot-loader/patch", "./src/index.tsx"],
+  },
+  mode: "development",
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json", ".sass", ".scss", ".css"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  devtool: "inline-source-map",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].[contenthash].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader",
+          {
+            loader: "postcss-loader",
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgoConfig: {
+                plugins: {
+                  removeUselessStrokeAndFill: false,
+                  removeViewBox: false,
+                  prefixIds: false,
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader",
+      },
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react", "@babel/preset-typescript"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    open: true,
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    hot: true,
+    port: 3000,
+    watchContentBase: false,
+    progress: false,
+    clientLogLevel: "warning",
+  },
+};
