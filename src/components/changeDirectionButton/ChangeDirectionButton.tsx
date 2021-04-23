2804453a63f15f067b08f41114342fd51870ed61
ekuznetski <e.kuznetski@gmail.com>
2021-04-23 22:46:48 +0300

    [components] update ChangeDirectionButton

--- src/components/changeDirectionButton/ChangeDirectionButton.tsx
+++ src/components/changeDirectionButton/ChangeDirectionButton.tsx
@@ -1,6 +1,25 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { ActionCreators, EActionTypes } from "@store";
+import { useExchangeType } from "@hooks";
 import "./changeDirectionButton.scss";
 
 export function ChangeDirectionButton() {
-  return <div className="change-direction-button">||</div>;
+  const dispatch = useDispatch();
+  const { isSellExchangeType } = useExchangeType();
+
+  function changeDirection() {
+    dispatch(ActionCreators[EActionTypes.changeExchangeDirection]());
+  }
+  return (
+    <div
+      onClick={changeDirection}
+      role="button"
+      className={`change-direction-button ${
+        isSellExchangeType ? "sell" : "buy"
+      }`}
+    >
+      ↓
+    </div>
+  );
 }
