cf1a49d9861ac5bcd96a3c4a3aa2ff704e628d04
ekuznetski <e.kuznetski@gmail.com>
2021-04-26 15:15:04 +0300

    [tests] fix aliases and add proxy for styles

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,16 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig.json");
+
+const paths = pathsToModuleNameMapper(compilerOptions.paths, {
+  prefix: "<rootDir>/src",
+});
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFiles: ["./src/jest/test-setup.js"],
   setupFilesAfterEnv: ["./src/jest/setupTests.ts"],
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+    ...paths,
+  },
 };
